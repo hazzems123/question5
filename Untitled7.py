0d7b5bdc95d0be73e19b7ba511bc11618095a834
hazzems123 <hazzems@hotmail.com>
2021-06-10 21:40:59 +0200

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+arr = np.array([[0,1] ,[2,3],[4,5]] )
+
+
+# In[4]:
+
+
+print (arr)
+
+
+# In[5]:
+
+
+arr= arr.tolist()
+
+
+# In[6]:
+
+
+print (arr)
+
+
+# In[ ]:
+
+
+
+
